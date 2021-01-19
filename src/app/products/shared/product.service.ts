15298bf049f152ff7fd3170712e1a5c72bbec856
atthana-p <atthana.p@codium.co>
2021-01-20 00:46:03 +0700

    22. Rxjs การประกาศและเรียกใช้นะ อยู่ใน Ep5.1, time 55:17 นะ ค่อนข้างยาก ต้องให้เข้าใจ แล้วก้อมาโชว์ผ่าน html ล่ะ

--- src/app/products/shared/product.service.ts
+++ src/app/products/shared/product.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  productUrl = 'https://codingthailand.com/api/get_courses.php';
 
   constructor(private http: HttpClient) { }
 
-  getProduct(){}
+  getProduct(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.productUrl);
+  }
 
 }
